c5cedc806953276da18e2e54dc2134e538b1c9d8
Patryk Bieguszewski <pb@inframet.com>
2018-11-28 08:57:11 +0100

    fill frame

--- simple_plugin/src/gst_simple_plugin_src.c
+++ simple_plugin/src/gst_simple_plugin_src.c
@@ -25,6 +25,8 @@ static void gst_imple_plugin_src_dispose (GObject* object);
 static gboolean gst_simple_plugin_src_start(GstBaseSrc* bsrc);
 static gboolean gst_simple_plugin_src_stop (GstBaseSrc* src);
 static GstFlowReturn gst_simple_plugin_src_fill(GstPushSrc* src, GstBuffer* buf);
+static GstCaps* gst_simple_plugin_src_get_caps (GstBaseSrc* src, GstCaps* filter);
+static gboolean gst_simple_plugin_src_set_caps (GstBaseSrc* src, GstCaps* caps);
 
 static GstStaticPadTemplate gst_simple_plugin_src_template =
   GST_STATIC_PAD_TEMPLATE ("src",
@@ -34,6 +36,9 @@ static GstStaticPadTemplate gst_simple_plugin_src_template =
 );
 
 G_DEFINE_TYPE(GstSimplePluginSrc, gst_simple_plugin_src, GST_TYPE_PUSH_SRC);
+
+static void* rawFrame = NULL;
+static guint32 frameFill = 0;
 
 static void gst_simple_plugin_src_init(GstSimplePluginSrc* src)
 {
@@ -108,6 +113,9 @@ static void gst_simple_plugin_src_class_init(GstSimplePluginSrcClass* klass)
 
   gstbasesrc_class->start = gst_simple_plugin_src_start;
   gstbasesrc_class->stop = gst_simple_plugin_src_stop;
+  gstbasesrc_class->get_caps = gst_simple_plugin_src_get_caps;
+  gstbasesrc_class->set_caps = gst_simple_plugin_src_set_caps;
+
   gstpushsrc_class->fill = gst_simple_plugin_src_fill;
 }
 
@@ -158,6 +166,8 @@ static void gst_simple_plugin_src_get_property(
 static void gst_imple_plugin_src_dispose (GObject* object)
 {
   (void)object;
+  free(rawFrame);
+  frameFill = 0;
 }
 
 static gboolean gst_simple_plugin_src_start(GstBaseSrc* src)
@@ -175,10 +185,67 @@ static gboolean gst_simple_plugin_src_stop (GstBaseSrc* src)
 static GstFlowReturn gst_simple_plugin_src_fill(GstPushSrc* src, GstBuffer* buf)
 {
   GstSimplePluginSrc* simplePluginSrc = GST_SIMPLE_PLUGIN_SRC(src);
-
-  (void)buf;
   (void)simplePluginSrc;
+  GstMapInfo mapInfo;
+  if (!gst_buffer_map(buf, &mapInfo, GST_MAP_WRITE))
+    return GST_FLOW_ERROR;
+
+  memcpy (mapInfo.data, rawFrame, frameFill);
+
+  gst_buffer_unmap (buf, &mapInfo);
+
   return GST_FLOW_OK;
+}
+
+static GstCaps* gst_simple_plugin_src_get_caps (GstBaseSrc* src, GstCaps* filter)
+{
+  GstVideoInfo vinfo;
+  GstVideoFormat videoForamt = GST_VIDEO_FORMAT_RGB;
+  guint32 width = 800;
+  guint32 height = 600;
+
+  gst_video_info_init (&vinfo);
+  gst_video_info_set_format (&vinfo, videoForamt, width, height);
+
+  vinfo.fps_n = 30;
+  vinfo.fps_d = 1;
+  GstCaps* caps = gst_video_info_to_caps (&vinfo);
+
+  if (filter && caps)
+  {
+    GstCaps *tmp = gst_caps_intersect(caps, filter);
+    gst_caps_unref (caps);
+    caps = tmp;
+  }
+
+  GstSimplePluginSrc* simplePluginSrc = GST_SIMPLE_PLUGIN_SRC(src);
+
+  if (!rawFrame)
+  {
+    frameFill = width * height * 3;
+    rawFrame = malloc(frameFill * sizeof(guint8));
+  }
+
+  guint8* frame = (guint8*)rawFrame;
+  for (guint i = 0; i < frameFill; i += 3)
+  {
+    frame[i + 0] = (guint8)simplePluginSrc->r;
+    frame[i + 1] = (guint8)simplePluginSrc->g;
+    frame[i + 2] = (guint8)simplePluginSrc->b;
+  }
+
+  return caps;
+}
+
+static gboolean gst_simple_plugin_src_set_caps (GstBaseSrc* src, GstCaps* caps)
+{
+  GstVideoInfo vinfo;
+
+  gst_video_info_from_caps (&vinfo, caps);
+
+  gst_base_src_set_blocksize(src, (guint)GST_VIDEO_INFO_SIZE(&vinfo));
+
+  return TRUE;
 }
 
 static gboolean plugin_init(GstPlugin* plugin)
